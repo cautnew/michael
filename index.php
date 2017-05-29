0f6703327a3b8012c7ab673e8fbb4d0ae1fa0deb
Felipe Martins <felipedesmartins@gmail.com>
2017-05-29 19:14:58 -0300

    Atualizacao

---
+++ index.php
@@ -0,0 +1,37 @@
+﻿<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <title>Login - Vai Achar!!!</title>
+  
+  <meta charset="utf-8">
+  <meta name="author" content="Felipe de Sousa Martins - Claro BA/SE (92034302)">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  <link rel="stylesheet" type="text/css" href="css/pg_login.css">
+  <link href='fonts/google-fonts-kaushan-script.css' rel='stylesheet'>
+
+  <link rel="icon" href="img/logo/favicon_clarosqr.ico">
+</head>
+
+<body>
+
+  <center>
+  	<h1>Vai achar!</h1>
+  	<h4>Faça seu login abaixo</h4>
+  </center>  
+  <div class="login">
+    <form action="" method="post">
+      <input type="text" name="" value="" placeholder="Usuário" required autofocus> 
+      <input type="password" name="" value="" placeholder="Senha" required>
+      <p><a href="#">Esqueci minha senha</a></p>
+      <p><a href="#">Solicitar acesso</a></p>
+      <button type="submit">Login</button>
+    </form>
+  </div>
+  <footer>
+  	<h4>Desenvolvido por <a href="mailto:felipe.martins@claro.com.br">Felipe Martins</a></h4>
+  </footer> 
+
+</body>
+
+</html>
